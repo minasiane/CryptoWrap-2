20e29a71752185df636ede317ebe8ba910c3543e
TimsterMon <wernert1@sou.edu>
2017-05-29 12:55:31 -0700

    first demo, no var input

---
+++ demo/demo.js
@@ -0,0 +1,31 @@
+// demonstarte  encrypt/decrypt with more flaire from node
+
+	 
+	var crypto = require('crypto'),
+		algorithm = 'aes-256-gcm',
+		password = '3zTvzr3p67VC61jmV54rIYu1545x4TlY', 
+		iv = '60iP0h6vJoEa'
+	 
+	function encrypt(text) {
+		var cipher = crypto.createCipheriv(algorithm,password,iv)
+		var encrypted = cipher.update(text,'utf8','hex')
+		encrypted += cipher.final('hex');
+		var tag = cipher.getAuthTag();
+		return {
+			content: encrypted,
+			tag: tag
+		};
+	}
+	 
+	function decrypt(encrypted) {
+		var decipher = crypto.createDecipheriv(algorithm,password,iv)
+		decipher.setAuthTag(encrypted.tag);
+		var dd = decipher.update(encrypted.content, 'hex', 'utf8')
+		dd += decipher.final('utf8');
+		return dd;
+	}
+	 
+	var ex = encrypt("Tim and Emily Rule!")
+
+	// outputs test text
+	console.log("\n Encrypted = " + ex.content + "\n Decrypted = " + decrypt(ex));
